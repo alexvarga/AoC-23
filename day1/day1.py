c23254519fa190f9444c25d3d8d986a8a32ce3f4
Aleksandra <aleksandra.varga@yahoo.com>
2023-12-01 22:12:38 +0100

    initial commit and Day 1 complete

---
+++ day1/day1.py
@@ -0,0 +1,18 @@
+import re
+sum = 0
+with open('day1/input.txt') as file:
+    lines = file.readlines()
+
+for line in lines:
+    line = line.replace('one', 'o1e').replace('two', 't2o').replace('three', 't3e').replace('four', 'f4r').replace('five', 'f5e').replace('six', 's6x').replace('seven', 's7n').replace('eight', 'e8t').replace('nine', 'n9e').replace('zero', 'z0o')
+    line = re.sub("[^0-9]", "", line)
+    if len(line)<2:
+        line = line+line
+        sum+=int(line)
+    elif len(line)>2:
+        line = line[0]+line[-1]
+        sum+=int(line)
+    else:
+        sum+=int(line)
+
+print(sum)
