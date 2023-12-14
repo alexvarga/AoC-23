86b4320da3e3939df4106e89fa667f7b7b40440f
Aleksandra <aleksandra.varga@yahoo.com>
2023-12-14 22:17:34 +0100

    Day 6 completed.

--- day6/day6.py
+++ day6/day6.py
@@ -1,25 +1,28 @@
 import os
+from tqdm import tqdm
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
 with open(dir_path+'/input.txt', 'r') as file:
     lines = file.read()
 
-time= lines.split('\n')[0].split(':')[1].split()
-distances= lines.split('\n')[1].split(':')[1].split()
+time= lines.split('\n')[0].split(':')[1].replace(" ", "")
+distances= lines.split('\n')[1].split(':')[1].replace(" ", "")
 
 print(time)
 print(distances)
 results = []
 result = 1
-for i in range(0, len(time)):
-    outcomes =[]
+# for i in range(0, len(time)):
+outcomes =[]
 
-    for j in range(1, int(time[i])):
-        outcomes.append(j*(int(time[i])-j))
+for j in tqdm(range(1, int(time))):
+
+    # print(j)
+    outcomes.append(j*(int(time)-j))
 
 
-    wins=len([item for item in outcomes if item > int(distances[i])])
-    results.append(wins)
-    result = result * wins
+wins=len([item for item in tqdm(outcomes) if item > int(distances)])
+results.append(wins)
+result = result * wins
 
 print(result)
